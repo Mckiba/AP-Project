5147195762c3148b4f8481f6110a7b00bf20aa0e
Jovaughn229 <56566585+Jovaughn229@users.noreply.github.com>
2022-03-23 21:20:36 -0500

    Crud update

--- Micro-Star/src/controller/CRUD.java
+++ Micro-Star/src/controller/CRUD.java
@@ -1,5 +1,35 @@
 package controller;
 
-public class CRUD {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 
+public class CRUD {
+	
+	private static Connection connect = null;
+	private Statement statement = null;
+	private ResultSet resultSet = null;
+	
+	public Connection getDatabaseConnection()
+	{
+		if(connect == null) {
+			try
+			{
+				String url = "jdbc:mysql://localhost:3306/employee";
+				connect = DriverManager.getConnection(url, "root","");
+				
+				JOptionPane.showMessageDialog(null,"DB Connection Established", "CONNECTON STATUS", JOptionPane.INFORMATION_MESSAGE);
+				
+			}
+			catch(SQLException ex)
+			{
+				JOptionPane.showMessageDialog(null,"Could not connect to database\n" + ex, "Connection Failure", JOptionPane.ERROR_MESSAGE);
+				
+			}
+		}
+		return connect;
+	}
 }
